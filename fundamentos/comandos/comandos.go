b382af59b4d20c4808bb80131a46e39e4158f24f
saulotarsobc <saulotarsobc@gmail.com>
2024-08-16 16:54:43 -0300

    new file:   constvar/constvar.go new file:   fundamentos/comandos/comandos.go renamed:    .vscode/extensions.json -> fundamentos/primeiro/.vscode/extensions.json new file:   fundamentos/primeiro/primeiro.go

---
+++ fundamentos/comandos/comandos.go
@@ -0,0 +1,7 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Printf("Outro porgrma em %s!\n")
+}
---
+++ fundamentos/primeiro/primeiro.go
@@ -0,0 +1,14 @@
+// progrmas execultaveis iniciando pelo main
+package main
+
+/*
+os codigos em go são organicado em pacores
+e para usa-los é nescessario o import
+*/
+import "fmt"
+
+// A porta de entrada de um programa em go é a funçao main
+func main() {
+	fmt.Println("Primeiro Programa em Go")
+	fmt.Println("Primeiro Programa em Go. Ola, mundo!")
+}
