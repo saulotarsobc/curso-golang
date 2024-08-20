9297ffdd21c4b5e3c299ac67c914c3d87151bc31
saulotarsobc <saulotarsobc@gmail.com>
2024-08-20 20:34:49 -0300

    funcoes

---
+++ fundamentos/funcoes/funcoes.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+// somaa returns the sum of two integers.
+//
+// Parameters:
+//  a (int): The first integer to be added.
+//  b (int): The second integer to be added.
+// Return type:
+//  int: The sum of a and b.
+func somaa(a int, b int) int {
+	return a + b
+}
+
+// print prints the value of an integer to the console.
+//
+// Parameter:
+//  valor (int): The integer value to be printed.
+// Return type:
+//  None
+func imprimir(valor int) {
+	fmt.Println("valor: ", valor)
+}
---
+++ fundamentos/funcoes/main.go
@@ -0,0 +1,11 @@
+package main
+
+// main is the entry point of the Go program.
+//
+// It calls the somaa function with two integer arguments and prints the result.
+// No return type.
+func main() {
+	somaa(3, 4)
+	resultado := somaa(1, 2)
+	imprimir(resultado)
+}
